0a85f8530c255996d63485fad3835d48c5a17db5
AbhijitSarkaar <abhijit.sarkaar26@gmail.com>
2023-09-20 08:37:06 +0530

    changes

--- src/server.js
+++ src/server.js
@@ -1,5 +1,5 @@
 const express = require('express')
-const userRoutes = require('./routes/users')
+// const userRoutes = require('./routes/users')
 
 const app = express()
 
@@ -7,7 +7,7 @@ app.get('/', (req, res) => {
     res.send('hello world')
 })
 
-app.get('/users', userRoutes)
+// app.get('/users', userRoutes)
 
 app.listen(3000, () => {
     console.log('listening to port 3000')
